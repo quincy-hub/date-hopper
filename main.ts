84eed8d502aafe03c89e15d101de3d425b7ba940
Jason Haslow <jasonhaslow@gmail.com>
2023-08-21 20:55:56 -0400

    add four commands for adjust Date

--- main.ts
+++ main.ts
@@ -1,151 +1,96 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MomentInput } from 'moment';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, moment } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class QuincyFirstPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		new Notice("Hello")
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is the best notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addRibbonIcon("calendar-days", "Awesomenes", () => {
+			new Notice('Hi from ribbon')
+		})
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Why');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
+			id: "next-day",
+			name: "Add a day",
+			editorCallback: (editor, view) => {
+				this.adjustDate(editor, 'days', 1)
+			},
+		})
+
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				console.log(App)
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+			id: "previous-day",
+			name: "Subtract a day",
+			editorCallback: (editor, view) => {
+				this.adjustDate(editor, 'days', -1)
+			},
+		})
+
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleComplexModal(this.app).open();
-					}
+			id: "add-week",
+			name: "Add a week",
+			editorCallback: (editor, view) => {
+				this.adjustDate(editor, 'days', 7)
+			},
+		})
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+		this.addCommand({
+			id: "subtract-week",
+			name: "Subtract a week",
+			editorCallback: (editor, view) => {
+				this.adjustDate(editor, 'days', -7)
+			},
+		})
+	}
+
+	async onunload() {
+
+	}
+
+	adjustDate(editor: Editor, interval: moment.unitOfTime.DurationConstructor, value: moment.DurationInputArg1){
+		const cursor = editor.getCursor()
+		const lineStr = editor.getLine(cursor.line)
+		const reg = /\d{4}-\d{2}-\d{2}/gm;
+		const datesOnLine = lineStr.match(reg)
+		console.log('datesOnLine', datesOnLine)
+
+		const positionsOnLine = datesOnLine?.map(d => {
+			let ret = {
+				from: {
+					line: cursor.line,
+					ch: lineStr.indexOf(d)
+				}, 
+				to: {
+					line: cursor.line,
+					ch: lineStr.indexOf(d) + d.length
+				},
+				cursorInRange: false
 			}
-		});
+			ret.cursorInRange = cursor.ch >= ret.from.ch && cursor.ch <= ret.to.ch
+			return ret	
+		}) || []
+		console.log(cursor)
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// TODO: Handle multiselect
+		const [cursorDate] = positionsOnLine.filter(p => p.cursorInRange)
+		console.log('cursorDate', cursorDate)
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		if (cursorDate){
+			editor.setSelection(cursorDate.from, cursorDate.to)
+			const selectedDate = editor.getSelection()
+			console.log('selectedDate', selectedDate)
+			const replaceValue = this.addToDate(selectedDate, interval, value)
+			editor.replaceSelection(replaceValue)
+		}
+		else {
+			new Notice(`No Date found under cursor.`, 3000)
+		}
 	}
 
-	onunload() {
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Fanciest Modal You have ever seen!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleComplexModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Complexiest Modal You have ever seen!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	addToDate(dateStr: MomentInput, interval: moment.unitOfTime.DurationConstructor, value: moment.DurationInputArg1) {
+		const mDate = moment(dateStr)
+		mDate.add(value, interval)
+		const returnStr = mDate.format('YYYY-MM-DD')
+		return returnStr
 	}
 }
